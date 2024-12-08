46917636db54f8d5f14eb7aa035c05dda2c79552
Yasin Mia palash <yasinmiapolash@gmail.com>
2024-12-08 17:52:24 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ Future<void> _setup() async {
 Android
   // step1: minSDk 21--app/build.gradle
   // step2:Check kotlin version--settings.gradle
-  //step3:  <style name="NormalTheme" parent="Theme.MaterialComponents">-main/res/values/styles.xml(add this same value night/styles.xml)
+
+  //step3:  <style name="NormalTheme" parent="Theme.MaterialComponents">----android/app/src/main/res/values/styles.xml(add this same ----value night/styles.xml)
   //step4:FlutterFragmentActivity -- mainActivity.kt
   //step5:add proguard-rules.pro--android/app
   //step6:initialized
